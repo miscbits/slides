947b6288865f1f8e7666f8a08619bcfa11d93295
miscbits <willow6786@gmail.com>
2019-06-08 14:27:31 -0400

    initial commit

---
+++ database/migrations/2019_06_08_180522_create_slides_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSlidesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slides', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('slides', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('content');
+            $table->string('slug')->unique();
+            $table->unsignedInteger('section_id');
+            $table->timestamps();
+
+            $table->foreign('section_id')
+                ->references('id')
+                ->on('sections');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slides');
+    }
+}
